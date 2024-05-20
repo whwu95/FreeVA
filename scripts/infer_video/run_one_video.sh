d00b0511210d5f4c6642ab000f9a379f73a399f4
whwu95 <whwu.ucas@gmail.com>
2024-05-20 16:13:54 +0800

    update

--- scripts/infer_video/run_one_video.sh
+++ scripts/infer_video/run_one_video.sh
@@ -1,6 +1,6 @@
-CKPT_NAME="llava-v1.5-13b"
-num_frames=8
-model_path="ckpt/llava-v1.5-13b"
+CKPT_NAME="llava-v1.5-7b"
+num_frames=4
+model_path="ckpt/llava-v1.5-7b"
 video_path="video_samples/sample_demo_23.mp4"
 
 CUDA_VISIBLE_DEVICES=0 python3 llava/eval/single_video_inference.py \
